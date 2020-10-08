3d6045511b278d44481c589c33047feaedbd57a3
Bikash <bikash.gurung@dcpl.bt>
2020-10-08 12:12:27 +0600

    Home component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,14 @@ import { NgModule } from '@angular/core';
 import {AppComponent} from './app.component';
 import {Route, RouterModule} from '@angular/router';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
 
 
 const route: Route[] = [
+  {
+    path: '',
+    component: HomeComponent
+  },
   {
     path: 'drone',
     loadChildren: () => import('./drone/drone.module').then(m => m.DroneModule)
@@ -14,12 +19,15 @@ const route: Route[] = [
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot(route)
+    RouterModule.forRoot(route, {
+      enableTracing: true
+    })
   ],
   providers: [HttpClient],
   bootstrap: [AppComponent]
